a1aec06f43a7ce139048f0500522d07b70791271
PatriceKast <33693800+PatriceKast@users.noreply.github.com>
2019-07-07 16:45:27 +0200

    Update Device.ts
    Added Devices

--- src/Device.ts
+++ src/Device.ts
@@ -8,7 +8,11 @@ const DeviceTypes: { [key: string]: Device } = {
   DEFAULT: { name: "Default", ports: new Set() },
   ROUTER: { name: "Router", ipsuffix: "1", ports: new Set([80, 443]) },
   SYNOLOGY_NAS: { name: "Synology NAS", ports: new Set([5000, 5001]) },
-  DATABASE: { name: "Database", ports: new Set([3306]) }
+  DATABASE: { name: "Database", ports: new Set([3306]) },
+  MAIL: { name: "Mail Server", ports: new Set([25, 110, 143, 465, 587, 993, 995]) },
+  LDAP: { name: "LDAP Server", ports: new Set([389, 636]) },
+  KERBEROS: { name: "Kerberos Server", ports: new Set([88, 464, 543, 544, 749, 750, 751, 752, 753, 754, 760, 1109, 2053, 2105]) },
+  AD: { name: "Active Directory", ports: new Set([445]) }
 };
 
 export const getType = (ip: string, ports: Set<number>) => {
